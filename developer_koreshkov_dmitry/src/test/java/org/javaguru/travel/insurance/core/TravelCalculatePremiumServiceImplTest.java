9a334fd25a64c7ea385a6c916cbcee553310b072
dkoreshkov <d.koreshkov88@gmail.com>
2025-04-18 16:44:45 +0300

    STEP 130 - DONE

--- developer_koreshkov_dmitry/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ developer_koreshkov_dmitry/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -1,12 +1,31 @@
 package org.javaguru.travel.insurance.core;
 
+import org.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
+import org.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
 import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TravelCalculatePremiumServiceImplTest {
 
-    @Test
-    public void deleteMe() {
+    TravelCalculatePremiumServiceImpl service = new TravelCalculatePremiumServiceImpl();
 
+    @Test
+    public void calculatePremiumResponseTest() {
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
+        request.setPersonFirstName("Jimmy");
+        request.setPersonLastName("Dory");
+        request.setAgreementDateFrom(new Date());
+        request.setAgreementDateTo(new Date());
+
+        TravelCalculatePremiumResponse response = service.calculatePremium(request);
+
+        assertEquals(response.getPersonFirstName(), request.getPersonFirstName());
+        assertEquals(response.getPersonLastName(), request.getPersonLastName());
+        assertEquals(response.getAgreementDateFrom(), request.getAgreementDateFrom());
+        assertEquals(response.getAgreementDateTo(),request.getAgreementDateTo());
     }
 
 }
